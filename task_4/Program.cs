bcc92e67373d3929b2e51374da636c78b5424fc6
Umid <spec7575@mail.ru>
2022-04-26 14:53:49 +0500

    Task 4

---
+++ task_4/Program.cs
@@ -0,0 +1,16 @@
+﻿string first = Console.ReadLine();
+string second = Console.ReadLine();
+string third = Console.ReadLine();
+int f = int.Parse(first);
+int s = int.Parse(second);
+int t = int.Parse(third);
+
+if(f>s & f>t){
+Console.WriteLine(f + " Максимальное число");
+}
+if(s>f & s>t){
+Console.WriteLine(s + " Максимальное число");
+}
+if(t>f & t>s){
+Console.WriteLine(t + " Максимальное число");
+}
