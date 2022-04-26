30dc3670d4bb7e601180084d1da8c2268f84dd61
Umid <spec7575@mail.ru>
2022-04-26 14:52:20 +0500

    Task 3

---
+++ task_3/Program.cs
@@ -0,0 +1,10 @@
+﻿string first = Console.ReadLine();
+string second = Console.ReadLine();
+int f = int.Parse(first);
+int s = int.Parse(second);
+
+if(f>s){
+    Console.WriteLine("Число " + f + " больше чем " + s);
+}else{
+    Console.WriteLine("Число " + s + " больше чем " + f);
+}
