ef8cb0bf326ab595506c8561320ab17a9be434d2
Umid <spec7575@mail.ru>
2022-04-26 14:55:26 +0500

    Task 5

---
+++ task_5/Program.cs
@@ -0,0 +1,7 @@
+﻿string first = Console.ReadLine();
+int f = int.Parse(first);
+if(f%2 == 0){
+    Console.WriteLine("Число " + f + " явлается чётным");
+}else{
+    Console.WriteLine("Число " + f + " явлается нечётным");
+}
